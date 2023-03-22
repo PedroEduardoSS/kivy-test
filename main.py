f08c7e9d098778f396bbb5959d736a6aa85c8858
PedroEduardoSS <pedroeduardossousa@gmail.com>
2023-03-22 11:58:11 -0300

    create comparator app

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class Comp(BoxLayout):
+    def calc(self):
+        pr1 = float(self.ids.pr1.text)
+        ps1 = float(self.ids.ps1.text)
+        pr2 = float(self.ids.pr2.text)
+        ps2 = float(self.ids.ps2.text)
+
+        self.ids.p1.text = str(pr1/ps1*1000)
+        self.ids.p2.text = str(pr2/ps2*1000)
+
+        self.ids.dif.text = str( (pr1/ps1) / (pr2/ps2) * 100) + ' %'
+
+class Main(App):
+    def build(self):
+        return Comp()
+    
+Main().run()
